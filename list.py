ce78f65d21f6882308e7cc1d6be939c3d60df392
vituchin <vic2018dev1@gmail.com>
2017-12-16 01:40:45 -0500

    updated version of linkedlist

--- list.py
+++ list.py
@@ -12,3 +12,30 @@ class Node(object):
 
     def set_next(self, new_next):
 self.next = new_next
+
+class LinkedList(object):
+
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.get_next()
+        return count
+
+    def search(self, data):
+        current = self.head
+        while current:
+            if current.get_data() == data:
+                return current
+            else:
+                current = current.get_next()
+return None
