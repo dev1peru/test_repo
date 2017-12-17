671c796b5a56c6f5812407cc78b34f613e2345b3
vituchin <vic2018dev1@gmail.com>
2017-12-17 07:53:27 -0500

    added merge sort fuction

--- list.py
+++ list.py
@@ -39,3 +39,36 @@ class LinkedList(object):
             else:
                 current = current.get_next()
 return None
+
+def delete(self, data):
+        current = self.head
+        prev = None
+        while current:
+            if current.get_data() == data:
+                if current == self.head:
+                    self.head = current.get_next()
+                else:
+                    prev.set_next(current.get_next())
+                return current
+            prev = current
+            current = current.get_next()
+        return None
+
+    def print(self):
+        lst = []
+        current = self.head
+        while current:
+            lst.append(str(current.get_data()))
+            current = current.get_next()
+print('->'.join(lst))
+
+# merge sort 12-17-2018
+def mergesort(x):
+    """ Function to sort an array using merge sort algorithm """
+    if len(x) == 0 or len(x) == 1:
+        return x
+    else:
+        middle = len(x)/2
+        a = mergesort(x[:middle])
+        b = mergesort(x[middle:])
+return merge(a,b)
